0f5014601aa0016e99effb712a8be5f8e776e100
Uzair Ahmed Kayani <uzairahmedkayani@gmail.com>
2025-10-01 16:11:19 +0500

    Added Portal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useImperativeHandle, useRef } from "react";
+import {createPortal} from 'react-dom'
 
 export default function ResultModal({ ref, targetTime, timeRemaining, onReset }) {
   const dialog = useRef();
@@ -12,7 +13,7 @@ export default function ResultModal({ ref, targetTime, timeRemaining, onReset })
       },
     };
   });
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You lost </h2>}
       {!userLost && <h2>Your Score: {score}</h2>}
@@ -26,6 +27,7 @@ export default function ResultModal({ ref, targetTime, timeRemaining, onReset })
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
   );
 }
