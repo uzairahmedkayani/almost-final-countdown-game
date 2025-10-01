eb6b4e1f813956288796c8f43c086a20409eff84
Uzair Ahmed Kayani <uzairahmedkayani@gmail.com>
2025-10-01 12:48:48 +0500

    Game Implemented

---
+++ src/components/Player.jsx
@@ -0,0 +1,18 @@
+import { useState, UseRef, useRef } from "react";
+
+export default function Player() {
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
+  }
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? "Unknown Entity"}</h2>
+      <p>
+        <input type="text" ref={playerName} />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,31 @@
+import { useImperativeHandle, useRef } from "react";
+
+export default function ResultModal({ ref, targetTime, timeRemaining, onReset }) {
+  const dialog = useRef();
+  const userLost = timeRemaining<=0;
+  const formattedTime = (timeRemaining/1000).toFixed(2);
+  const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100)
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+  return (
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You lost </h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
+
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with <strong>{formattedTime} seconds left.</strong>
+      </p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+}
